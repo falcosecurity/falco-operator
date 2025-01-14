2757e801e1dfaca650275d6d71a061e6fd68a600
Aldo Lacuku <aldo@lacuku.eu>
2025-01-14 11:03:47 +0100

    update: change domain to falcosecurity.dev
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -16,7 +16,7 @@ limitations under the License.
 
 // Package v1alpha1 contains API Schema definitions for the instance v1alpha1 API group.
 // +kubebuilder:object:generate=true
-// +groupName=instance.falco.org
+// +groupName=instance.falcosecurity.dev
 package v1alpha1
 
 import (
@@ -26,7 +26,7 @@ import (
 
 var (
 	// GroupVersion is group version used to register these objects.
-	GroupVersion = schema.GroupVersion{Group: "instance.falco.org", Version: "v1alpha1"}
+	GroupVersion = schema.GroupVersion{Group: "instance.falcosecurity.dev", Version: "v1alpha1"}
 
 	// SchemeBuilder is used to add go types to the GroupVersionKind scheme.
 	SchemeBuilder = &scheme.Builder{GroupVersion: GroupVersion}
