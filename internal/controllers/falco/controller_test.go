e662fe8bc28da09d862c9c4f411ad046c3af7c90
Aldo Lacuku <aldo@lacuku.eu>
2025-01-20 12:51:06 +0100

    new(test): add unit tests for falco CRD default values
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- internal/controllers/falco/controller_test.go
+++ internal/controllers/falco/controller_test.go
@@ -29,7 +29,7 @@ import (
 	instancev1alpha1 "github.com/alacuku/falco-operator/api/v1alpha1"
 )
 
-var _ = Describe("Falco Controller", func() {
+var _ = Describe("Falco Controller", Ordered, func() {
 	Context("When reconciling a resource", func() {
 		const resourceName = "test-resource"
 
@@ -51,7 +51,6 @@ var _ = Describe("Falco Controller", func() {
 						Namespace: "default",
 					},
 					Spec: instancev1alpha1.FalcoSpec{
-						Type:            "daemonset",
 						Replicas:        nil,
 						PodTemplateSpec: nil,
 					},
@@ -82,6 +81,77 @@ var _ = Describe("Falco Controller", func() {
 			Expect(err).NotTo(HaveOccurred())
 			// TODO(user): Add more specific assertions depending on your controller's reconciliation logic.
 			// Example: If you expect a certain status condition after reconciliation, verify it here.
+		})
+	})
+
+	Context("When creating an empty CRD", func() {
+		const resourceName = "empty-crd"
+
+		ctx := context.Background()
+
+		typeNamespacedName := types.NamespacedName{
+			Name:      resourceName,
+			Namespace: "default",
+		}
+		falco := &instancev1alpha1.Falco{}
+
+		BeforeAll(func() {
+			By("creating the custom resource for the Kind Falco")
+			err := k8sClient.Get(ctx, typeNamespacedName, falco)
+			if err != nil && errors.IsNotFound(err) {
+				resource := &instancev1alpha1.Falco{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      resourceName,
+						Namespace: "default",
+					},
+					Spec: instancev1alpha1.FalcoSpec{
+						Replicas:        nil,
+						PodTemplateSpec: nil,
+					},
+				}
+				Expect(k8sClient.Create(ctx, resource)).To(Succeed())
+			}
+		})
+
+		AfterAll(func() {
+			resource := &instancev1alpha1.Falco{}
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+
+			By("Cleanup the specific resource instance Falco")
+			Expect(k8sClient.Delete(ctx, resource)).To(Succeed())
+		})
+
+		It("type should be set to daemonset", func() {
+			resource := &instancev1alpha1.Falco{}
+			By("Getting the created resource")
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resource.Spec.Type).To(BeEquivalentTo("daemonset"))
+		})
+
+		It("replicas should be set to nil", func() {
+			resource := &instancev1alpha1.Falco{}
+			By("Getting the created resource")
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resource.Spec.Replicas).To(BeNil())
+		})
+
+		It("version should be set to empty string", func() {
+			resource := &instancev1alpha1.Falco{}
+			By("Getting the created resource")
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resource.Spec.Version).To(BeEmpty())
+		})
+
+		It("podTemplateSpec should be set to nil", func() {
+			resource := &instancev1alpha1.Falco{}
+			By("Getting the created resource")
+			err := k8sClient.Get(ctx, typeNamespacedName, resource)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(resource.Spec.PodTemplateSpec).To(BeNil())
 		})
 	})
 })
