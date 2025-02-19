4e24329db7fcd14afedc6fd6ac99ca713a62a630
Aldo Lacuku <aldo@lacuku.eu>
2025-02-19 09:45:18 +0100

    new(falco): apply desired state when diverges from the current one
    Furthermore, it shows the changes that will be applied
    
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- internal/controllers/falco/controller.go
+++ internal/controllers/falco/controller.go
@@ -19,8 +19,12 @@ package falco
 import (
 	"context"
 
+	"gopkg.in/yaml.v3"
 	appsv1 "k8s.io/api/apps/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -68,23 +72,8 @@ func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 		return ctrl.Result{}, err
 	}
 
-	logger.Info("Generating apply configuration from user input")
-	applyConfig, err := generateApplyConfiguration(ctx, r.Client, falco)
-	if err != nil {
-		logger.Error(err, "unable to generate apply configuration")
-		return ctrl.Result{}, err
-	}
-
-	// Set owner reference.
-	if err = ctrl.SetControllerReference(falco, applyConfig, r.Scheme); err != nil {
-		logger.Error(err, "unable to set owner reference")
-		return ctrl.Result{}, err
-	}
-
-	// apply the patch
-	err = r.Patch(ctx, applyConfig, client.Apply, client.ForceOwnership, client.FieldOwner("falco-controller"))
-	if err != nil {
-		logger.Error(err, "unable to apply patch")
+	// Ensure the deployment/daemonset is created.
+	if err := r.ensureDeployment(ctx, falco); err != nil {
 		return ctrl.Result{}, err
 	}
 
@@ -134,4 +123,78 @@ func (r *Reconciler) handleDeletion(ctx context.Context, falco *instancev1alpha1
 		return true, nil
 	}
 	return false, nil
+}
+
+// ensureDeployment ensures the Falco deployment or daemonset is created or updated.
+// It returns an error if the operation fails.
+func (r *Reconciler) ensureDeployment(ctx context.Context, falco *instancev1alpha1.Falco) error {
+	logger := log.FromContext(ctx)
+
+	logger.Info("Generating apply configuration from user input")
+	applyConfig, err := generateApplyConfiguration(ctx, r.Client, falco)
+	if err != nil {
+		logger.Error(err, "unable to generate apply configuration")
+		return err
+	}
+
+	// transform the apply configuration to yaml.
+	applyConfigYaml, err := yaml.Marshal(applyConfig.Object)
+	if err != nil {
+		logger.Error(err, "unable to marshal apply configuration")
+		return err
+	}
+
+	logger.V(3).Info("Generated apply configuration", "yaml", string(applyConfigYaml))
+
+	// Set owner reference.
+	if err = ctrl.SetControllerReference(falco, applyConfig, r.Scheme); err != nil {
+		logger.Error(err, "unable to set owner reference")
+		return err
+	}
+
+	// Get existing Falco deployment/daemonset
+	existingResource := &unstructured.Unstructured{}
+	existingResource.SetGroupVersionKind(schema.GroupVersionKind{
+		Group:   "apps",
+		Version: "v1",
+		Kind:    falco.Spec.Type, // "Deployment" or "DaemonSet"
+	})
+
+	if err = r.Get(ctx, client.ObjectKeyFromObject(falco), existingResource); err != nil && !apierrors.IsNotFound(err) {
+		logger.Error(err, "unable to fetch existing resource")
+		return err
+	}
+
+	// If the resource has not been found, create it.
+	if apierrors.IsNotFound(err) {
+		logger.Info("Creating Falco resource", "kind", falco.Spec.Type)
+		err = r.Patch(ctx, applyConfig, client.Apply, client.ForceOwnership, client.FieldOwner("falco-controller"))
+		if err != nil {
+			logger.Error(err, "unable to apply patch")
+			return err
+		}
+		return nil
+	}
+
+	// If the resource has been found remove all the fields that are not needed.
+	removeUnwantedFields(existingResource)
+
+	cmp, err := diff(existingResource, applyConfig)
+	if err != nil {
+		logger.Error(err, "unable to compare existing and desired resources", "existing", existingResource, "desired", applyConfig)
+		return err
+	}
+
+	if cmp.IsSame() {
+		logger.Info("Falco resource is up to date", "kind", falco.Spec.Type)
+		return nil
+	}
+
+	logger.Info("Updating Falco resource", "kind", falco.Spec.Type, "diff", cmp.String())
+	if err = r.Patch(ctx, applyConfig, client.Apply, client.ForceOwnership, client.FieldOwner("falco-controller")); err != nil {
+		logger.Error(err, "unable to apply patch", "kind", falco.Spec.Type, "patch", applyConfig)
+		return err
+	}
+
+	return nil
 }
