20b263ce4b6ab77f6f6f420c81f670fbf7e6a55e
Aldo Lacuku <aldo@lacuku.eu>
2025-02-19 15:16:08 +0100

    new(falco): set the version in the CR if not specified by the user
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- internal/pkg/image/image.go
+++ internal/pkg/image/image.go
@@ -16,17 +16,26 @@
 
 package image
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 // BuildImageString constructs the image string from registry, repository, image, and tag.
 func BuildImageString(registry, repository, image, tag string) string {
 	return fmt.Sprintf("%s/%s/%s:%s", registry, repository, image, tag)
 }
 
+// BuildFalcoImageStringFromVersion constructs the image string for Falco.
 func BuildFalcoImageStringFromVersion(version string) string {
 	if version == "" {
 		return BuildImageString(Registry, Repository, FalcoImage, FalcoTag)
 	}
 
 	return fmt.Sprintf("%s/%s/%s:%s%s", Registry, Repository, FalcoImage, version, FalcoDebianImageSuffix)
+}
+
+// FalcoVersion returns the version of Falco specified in the FalcoTag.
+func FalcoVersion() string {
+	return strings.Split(FalcoTag, "-")[0]
 }
