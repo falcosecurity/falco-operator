c155cfcde3e9d7be0a757844a2a2ef04099cece0
Aldo Lacuku <aldo@lacuku.eu>
2025-03-12 12:18:48 +0100

    new(falco): add service for falco pods
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

---
+++ internal/controllers/falco/service.go
@@ -0,0 +1,91 @@
+// Copyright (C) 2025 The Falco Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package falco
+
+import (
+	"context"
+	"fmt"
+
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+
+	instancev1alpha1 "github.com/alacuku/falco-operator/api/v1alpha1"
+)
+
+// generateService returns a service for Falco.
+func generateService(ctx context.Context, cl client.Client, falco *instancev1alpha1.Falco) (*unstructured.Unstructured, error) {
+	svc := &corev1.Service{
+		TypeMeta: metav1.TypeMeta{
+			Kind:       "Service",
+			APIVersion: "v1",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      falco.Name,
+			Namespace: falco.Namespace,
+			Labels:    falco.Labels,
+		},
+		Spec: corev1.ServiceSpec{
+			Type: corev1.ServiceTypeClusterIP,
+			Ports: []corev1.ServicePort{
+				{
+					Name:       "web",
+					Protocol:   corev1.ProtocolTCP,
+					Port:       8765,
+					TargetPort: intstr.FromInt32(8765),
+				},
+			},
+			Selector: map[string]string{
+				"app.kubernetes.io/name":     falco.Name,
+				"app.kubernetes.io/instance": falco.Name,
+			},
+		},
+	}
+
+	// Set the controller as the owner of the Role
+	if err := controllerutil.SetControllerReference(falco, svc, cl.Scheme()); err != nil {
+		return nil, err
+	}
+
+	// Convert to unstructured object.
+	unstructuredObj, err := toUnstructured(svc)
+	if err != nil {
+		return nil, err
+	}
+
+	// Set the defaults by dry-run applying the object.
+	if err := setDefaultValues(ctx, cl, unstructuredObj, schema.GroupVersionKind{
+		Group:   corev1.GroupName,
+		Version: corev1.SchemeGroupVersion.Version,
+		Kind:    unstructuredObj.GetKind(),
+	}); err != nil {
+		return nil, err
+	}
+
+	// Set the name of the resource to the name of the falco CR.
+	if err := unstructured.SetNestedField(unstructuredObj.Object, falco.Name, "metadata", "name"); err != nil {
+		return nil, fmt.Errorf("failed to set name field: %w", err)
+	}
+
+	removeUnwantedFields(unstructuredObj)
+
+	return unstructuredObj, nil
+}
