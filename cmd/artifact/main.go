57f8e43a2aadf79052c0f1612717aade5f2e4d2e
Aldo Lacuku <aldo@lacuku.eu>
2025-03-19 10:00:25 +0100

    new(artifact-config): namespaced cache
    The controller handler config artifacts in its own namespace.
    
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- cmd/artifact/main.go
+++ cmd/artifact/main.go
@@ -27,6 +27,7 @@ import (
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/certwatcher"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
@@ -87,6 +88,13 @@ func main() {
 
 	setupLog.Info("Starting artifact operator", "version", version.SemVersion, "commit", version.GitCommit,
 		"buildDate", version.BuildDate, "compiler", version.Compiler, "platform", version.Platform)
+
+	// Get the namespace from environment variable.
+	namespace, ok := os.LookupEnv("POD_NAMESPACE")
+	if !ok {
+		setupLog.Error(nil, "unable to get POD_NAMESPACE environment variable")
+		os.Exit(1)
+	}
 
 	// if the enable-http2 flag is false (the default), http/2 should be disabled
 	// due to its vulnerabilities. More specifically, disabling http/2 will
@@ -184,6 +192,11 @@ func main() {
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "a7b8c9d0.falcosecurity.dev",
+		Cache: cache.Options{
+			DefaultNamespaces: map[string]cache.Config{
+				namespace: {},
+			},
+		},
 		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
 		// when the Manager ends. This requires the binary to immediately end when the
 		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
