b6c1d8faf201a247977a6fb09a38fe1087360999
Aldo Lacuku <aldo@lacuku.eu>
2025-03-25 12:04:53 +0100

    new(falco-pod): add rulesfile empty dir in falco pod
    The rulesfile empty dir is used by the artifact operator to
    configure the falco rules.
    
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

--- internal/controllers/falco/defaults.go
+++ internal/controllers/falco/defaults.go
@@ -112,6 +112,7 @@ var (
 		{Name: "containerd-socket", MountPath: "/host/run/containerd/"},
 		{Name: "crio-socket", MountPath: "/host/run/crio/"},
 		{Name: "falco-configs-crd", MountPath: mounts.ConfigDirPath},
+		{Name: mounts.RulesfileMountName, MountPath: mounts.RulesfileDirPath},
 	}
 
 	// DefaultFalcoVolumes are the default volumes for the Falco container.
@@ -128,6 +129,7 @@ var (
 		{Name: "crio-socket", VolumeSource: corev1.VolumeSource{HostPath: &corev1.HostPathVolumeSource{Path: "/run/crio"}}},
 		{Name: "proc-fs", VolumeSource: corev1.VolumeSource{HostPath: &corev1.HostPathVolumeSource{Path: "/proc"}}},
 		{Name: "falco-configs-crd", VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}}},
+		{Name: mounts.RulesfileMountName, VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{}}},
 	}
 
 	// DefaultFalcoConfig default Falco configuration.
@@ -297,6 +299,7 @@ webserver:
 				Name:      "falco-configs-crd",
 				MountPath: mounts.ConfigDirPath,
 			},
+			{Name: mounts.RulesfileMountName, MountPath: mounts.RulesfileMountName},
 		},
 		ReadinessProbe: &corev1.Probe{
 			InitialDelaySeconds: 5,
