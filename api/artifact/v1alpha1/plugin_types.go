3a0c68ea1d1674045dc397acfc31cda5aa15a2e3
Aldo Lacuku <aldo@lacuku.eu>
2025-04-15 14:32:06 +0200

    new(api): introduce plugin api
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

---
+++ api/artifact/v1alpha1/plugin_types.go
@@ -0,0 +1,65 @@
+// Copyright (C) 2025 The Falco Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+// Package controller defines controllers' logic.
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	commonv1alpha1 "github.com/falcosecurity/falco-operator/api/common/v1alpha1"
+)
+
+// PluginSpec defines the desired state of Plugin.
+type PluginSpec struct {
+	// OCIArtifact specifies the reference to an OCI artifact.
+	OCIArtifact *commonv1alpha1.OCIArtifact `json:"ociArtifact,omitempty"`
+	// Config specifies the configuration for the plugin.
+	Config *string `json:"config,omitempty"`
+}
+
+// PluginStatus defines the observed state of Plugin.
+type PluginStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "make" to regenerate code after modifying this file
+}
+
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=plugin
+
+// Plugin is the Schema for the plugin API.
+type Plugin struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   PluginSpec   `json:"spec,omitempty"`
+	Status PluginStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// PluginList contains a list of Plugin.
+type PluginList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Plugin `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Plugin{}, &PluginList{})
+}
