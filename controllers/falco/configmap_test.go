559304a1221c31446831fa9687ed05dac8eb0df3
Aldo Lacuku <aldo@lacuku.eu>
2025-05-14 10:09:49 +0200

    new(falco/tests): unit tests for configmap.go
    Signed-off-by: Aldo Lacuku <aldo@lacuku.eu>

---
+++ controllers/falco/configmap_test.go
@@ -0,0 +1,143 @@
+// Copyright (C) 2025 The Falco Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package falco
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	instancev1alpha1 "github.com/falcosecurity/falco-operator/api/instance/v1alpha1"
+)
+
+func TestGenerateConfigmap(t *testing.T) {
+	// Create a new scheme and add the necessary types
+	scheme := runtime.NewScheme()
+	_ = corev1.AddToScheme(scheme)
+	_ = instancev1alpha1.AddToScheme(scheme)
+
+	tests := []struct {
+		name        string
+		falco       *instancev1alpha1.Falco
+		wantErr     bool
+		errContains string
+	}{
+		{
+			name: "deployment type configmap",
+			falco: &instancev1alpha1.Falco{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-falco",
+					Namespace: "default",
+					Labels: map[string]string{
+						"app": "falco",
+					},
+				},
+				Spec: instancev1alpha1.FalcoSpec{
+					Type: resourceTypeDeployment,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "daemonset type configmap",
+			falco: &instancev1alpha1.Falco{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-falco",
+					Namespace: "default",
+					Labels: map[string]string{
+						"app": "falco",
+					},
+				},
+				Spec: instancev1alpha1.FalcoSpec{
+					Type: resourceTypeDaemonSet,
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "invalid type configmap",
+			falco: &instancev1alpha1.Falco{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-falco",
+					Namespace: "default",
+				},
+				Spec: instancev1alpha1.FalcoSpec{
+					Type: "invalid-type",
+				},
+			},
+			wantErr:     true,
+			errContains: "unsupported falco type",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Create a fake client
+			client := fake.NewClientBuilder().WithScheme(scheme).Build()
+
+			// Call the function
+			result, err := generateConfigmap(context.Background(), client, tt.falco)
+
+			if tt.wantErr {
+				assert.Error(t, err)
+				if tt.errContains != "" {
+					assert.Contains(t, err.Error(), tt.errContains)
+				}
+				assert.Nil(t, result)
+				return
+			}
+
+			assert.NoError(t, err)
+			assert.NotNil(t, result)
+
+			// Verify the basic structure of the configmap
+			name, _, err := unstructured.NestedString(result.Object, "metadata", "name")
+			assert.NoError(t, err)
+			assert.Equal(t, tt.falco.Name, name)
+
+			namespace, _, err := unstructured.NestedString(result.Object, "metadata", "namespace")
+			assert.NoError(t, err)
+			assert.Equal(t, tt.falco.Namespace, namespace)
+
+			// Verify the config data exists
+			data, _, err := unstructured.NestedStringMap(result.Object, "data")
+			assert.NoError(t, err)
+			assert.Contains(t, data, "falco.yaml")
+
+			// Verify config content based on type
+			expectedConfig := ""
+			switch tt.falco.Spec.Type {
+			case resourceTypeDeployment:
+				expectedConfig = deploymentFalcoConfig
+			case resourceTypeDaemonSet:
+				expectedConfig = daemonsetFalcoConfig
+			}
+			assert.Equal(t, expectedConfig, data["falco.yaml"])
+
+			// Verify labels
+			labels, _, err := unstructured.NestedStringMap(result.Object, "metadata", "labels")
+			assert.NoError(t, err)
+			assert.Equal(t, tt.falco.Labels, labels)
+		})
+	}
+}
