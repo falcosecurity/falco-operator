3eef4dce82b9fca229c62b36cbbe5ec7e9203064
alacuku <aldo@lacuku.eu>
2026-03-23 15:23:22 +0100

    fix(cmd): disable debug logging
    Signed-off-by: alacuku <aldo@lacuku.eu>

--- cmd/artifact/main.go
+++ cmd/artifact/main.go
@@ -66,6 +66,8 @@ func main() {
 	var secureMetrics bool
 	var enableHTTP2 bool
 	var tlsOpts []func(*tls.Config)
+	var opts zap.Options
+	
 	flag.StringVar(&metricsAddr, "metrics-bind-address", "0", "The address the metrics endpoint binds to. "+
 		"Use :8443 for HTTPS or :8080 for HTTP, or leave as 0 to disable the metrics service.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
@@ -83,9 +85,7 @@ func main() {
 	flag.StringVar(&metricsCertKey, "metrics-cert-key", "tls.key", "The name of the metrics server key file.")
 	flag.BoolVar(&enableHTTP2, "enable-http2", false,
 		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
-	opts := zap.Options{
-		Development: true,
-	}
+
 	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
 
