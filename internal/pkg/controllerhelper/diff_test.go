34b007ddc73da3a1bab840b91ce71226375b98cd
Chiman Jain <chimanjain15@gmail.com>
2026-06-30 16:43:08 +0530

    chore: upgrade sigs.k8s.io/structured-merge-diff to v6
    Signed-off-by: Chiman Jain <chimanjain15@gmail.com>

--- internal/pkg/controllerhelper/diff_test.go
+++ internal/pkg/controllerhelper/diff_test.go
@@ -24,8 +24,8 @@ import (
 	"github.com/stretchr/testify/require"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
-	"sigs.k8s.io/structured-merge-diff/v4/fieldpath"
-	"sigs.k8s.io/structured-merge-diff/v4/typed"
+	"sigs.k8s.io/structured-merge-diff/v6/fieldpath"
+	"sigs.k8s.io/structured-merge-diff/v6/typed"
 
 	"github.com/falcosecurity/falco-operator/internal/pkg/builders"
 )
